bd77422f8094eb182c22e184c0f52bfb086d575f
WiOrca <wiorcaman@gmail.com>
2020-09-25 22:14:52 -0400

    Fix executable path

--- app-startup.sh
+++ app-startup.sh
@@ -1,13 +1,6 @@
-#1 /bin/bash
-
-# Here is where you run the secure service you want to run.  Do not exit.  Run in foreground.
-# Overwrite this file in your docker image
-
-echo "IP information"
-
-ip addr
+#! /bin/bash
 
 echo "Starting Sonarr Service"
 
-mono /usr/lib/sonarr/bin/Sonarr.exe
+mono /opt/Sonarr/Sonarr.exe
 
